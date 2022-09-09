cc831b0294047978f0c0c20182ad3b70eb0abe0b
kivanval <kivanval@gmail.com>
2022-09-09 21:02:22 +0300

    L1.1 - Add Simple Controller

--- src/main/java/com/example/springdevkpi/SpringDevKpiApplication.java
+++ src/main/java/com/example/springdevkpi/SpringDevKpiApplication.java
@@ -1,12 +1,20 @@
 package com.example.springdevkpi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDevKpiApplication {
+public class SpringDevKpiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+        System.out.println("Begin of main");
         SpringApplication.run(SpringDevKpiApplication.class, args);
+        System.out.println("End of main");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello from Spring Boot!");
     }
 }
