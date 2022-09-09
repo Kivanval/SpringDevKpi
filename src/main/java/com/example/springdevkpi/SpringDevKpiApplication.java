2c09ae688d7a80ac790bbfb4ac417a1aa4129b0e
kivanval <kivanval@gmail.com>
2022-09-09 20:39:03 +0300

    Init commit

---
+++ src/main/java/com/example/springdevkpi/SpringDevKpiApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdevkpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevKpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDevKpiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springdevkpi/SpringDevKpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdevkpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevKpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
