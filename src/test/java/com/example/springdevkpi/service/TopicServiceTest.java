ae5eb7d279c69143b46c90d67b3ec734a058bfc8
Boros <1109bogdan@gmail.com>
2022-12-09 07:11:24 +0200

    readme lab 2 full ready

--- src/test/java/com/example/springdevkpi/service/TopicServiceTest.java
+++ src/test/java/com/example/springdevkpi/service/TopicServiceTest.java
@@ -1,20 +1,12 @@
 package com.example.springdevkpi.service;
 
-import com.example.springdevkpi.data.PostRepository;
+
 import com.example.springdevkpi.data.TopicRepository;
 import com.example.springdevkpi.data.UserRepository;
-import com.example.springdevkpi.web.data.transfer.PostAddPayload;
-import com.example.springdevkpi.web.data.transfer.TopicAddPayload;
-import com.example.springdevkpi.web.data.transfer.TopicUpdatePayload;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static com.example.springdevkpi.service.TestHelper.getPostForTest;
-import static com.example.springdevkpi.service.TestHelper.getUserForTest;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TopicServiceTest {
@@ -23,115 +15,24 @@ class TopicServiceTest {
     private TopicService topicService;
 
     @Autowired
-    private PostRepository postRepository;
-
-    @Autowired
-    private TopicRepository topicRepository;
+    private UserService userService;
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TopicRepository topicRepository;
+
     @BeforeEach
     void setUp() {
-        postRepository.deleteAll();
-        topicRepository.deleteAll();
         userRepository.deleteAll();
-        userRepository.save(getUserForTest());
+        topicRepository.deleteAll();
     }
 
     @AfterEach
     void tearDown() {
-        postRepository.deleteAll();
         topicRepository.deleteAll();
-        userRepository.deleteAll();
-    }
-
-    @Test
-    void shouldCreate() {
-        var payload = new TopicAddPayload("test", "test", "TESTER");
-
-        var topic = topicService.create(payload);
-
-        assertTrue(topicRepository.existsById(topic.getId()));
-    }
-
-    @Test
-    void shouldSetFieldsFromPayload() {
-        var payload = new TopicAddPayload("test", "test", "TESTER");
-
-        var topic = topicService.create(payload);
-
-        assertEquals("test", topic.getTitle());
-        assertEquals("test", topic.getDescription());
-        assertEquals("TESTER", topic.getCreator().getUsername());
     }
 
 
-    @Test
-    void shouldFindById() {
-        var payload = new TopicAddPayload("test", "test", "TESTER");
-
-        var topic = topicService.create(payload);
-
-        assertTrue(topicService.findById(topic.getId()).isPresent());
-    }
-
-    @Test
-    void shouldNotFindById() {
-        var payload = new TopicAddPayload("test", "test", "TESTER");
-
-        var topic = topicService.create(payload);
-
-        assertTrue(topicService.findById(topic.getId() - 1).isEmpty());
-    }
-
-    @Test
-    void shouldFindAll() {
-        var payload1 = new TopicAddPayload("test", "test", "TESTER");
-        var payload2 = new TopicAddPayload("test", "test", "TESTER");
-
-        topicService.create(payload1);
-        topicService.create(payload2);
-
-        assertEquals(2, topicService.findAll(0, 2, "id").getSize());
-    }
-
-    @Test
-    void shouldDeleteById() {
-        var payload = new TopicAddPayload("test", "test", "TESTER");
-
-        var topic = topicService.create(payload);
-        topicService.deleteById(topic.getId());
-
-        assertFalse(topicRepository.existsById(topic.getId()));
-    }
-
-    @Test
-    void shouldDeleteByIdAllRelatedPosts() {
-        var payload = new TopicAddPayload("test", "test", "TESTER");
-        var post = getPostForTest();
-
-        var topic = topicService.create(payload);
-        topic.addPost(post);
-        topicService.deleteById(topic.getId());
-
-        assertEquals(0, postRepository.count());
-        assertTrue(userRepository.existsByUsername("TESTER"));
-    }
-
-
-    @Test
-    void shouldUpdateTitleAndDescFromPayload() {
-        var payload = new TopicAddPayload("test", "test", "TESTER");
-        var updatePayload = new TopicUpdatePayload("new test", "new test");
-
-        var topic = topicService.create(payload);
-        var isUpdated = topicService.update(updatePayload, topic.getId());
-        var updatedTopic = topicService.findById(topic.getId())
-                .orElseThrow(AssertionError::new);
-
-        assertTrue(isUpdated);
-        assertEquals("new test", updatedTopic.getTitle());
-        assertEquals("new test", updatedTopic.getDescription());
-    }
 }
